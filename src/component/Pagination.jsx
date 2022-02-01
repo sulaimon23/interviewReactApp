16240d1add6cb62adfd82304b32747283d392436
Taiwo Sulaiman <tsulaimon96@gmail.com>
2022-02-01 11:46:03 +0100

    netlify build fixes

--- src/component/Pagination.jsx
+++ src/component/Pagination.jsx
@@ -35,18 +35,18 @@ const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage, setPostPe
             </div>
             <ul className="flex items-center ">
                 <li>
-                    <a href="!#" onClick={() => setPrev()} className="block mr-2 py-1.5 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    <a onClick={() => setPrev()} className="block mr-2 py-1.5 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                         <span className="sr-only">Previous</span>
                         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
                     </a>
                 </li>
                 <li className="rounded-lg overflow-hidden border w-full border-gray-700 flex justify-between h-full " >
                     {pageNumbers.map(number => (
-                        <a key={number} onClick={() => setPageNumbers(number)} href="!#" className={`${currentPage === number ? "" : "dark:bg-gray-800"} w-full py-2 h-full px-3  text-gray-500 bg-white  border-gray-300  hover:text-gray-700  dark:border-gray-700 dark:text-gray-400  dark:hover:text-gray-700`}>{number}</a>
+                        <a key={number} onClick={() => setPageNumbers(number)} className={`${currentPage === number ? "" : "dark:bg-gray-800"} w-full py-2 h-full px-3  text-gray-500 bg-white  border-gray-300  hover:text-gray-700  dark:border-gray-700 dark:text-gray-400  dark:hover:text-gray-700`}>{number}</a>
                     ))}
                 </li>
                 <li>
-                    <a href="!#" onClick={() => setNext()} className="block ml-2 py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    <a onClick={() => setNext()} className="block ml-2 py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                         <span className="sr-only">Next</span>
                         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
                     </a>
